160b8c8505e770cbb303e83f45d67d2c078f4e8e
Madugba Emmanuel Chinonso <nwadubaemmanuel@gmail.com>
2021-11-19 05:28:59 -0800

    :tada: First Commit

---
+++ public/script.js
@@ -0,0 +1,43 @@
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+const myPeer = new Peer(undefined, {
+    host:'/',
+    port: '3001'
+})
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+if(navigator.mediaDevices.getUserMedia){
+
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      }).then((stream) => {
+        addVideoStream(myVideo, stream)
+    }).catch(err=>{
+        console.log(err)
+    })
+
+}
+
+
+myPeer.on('open', id=>{
+
+    socket.emit('join-room', ROOM_ID, id)
+
+})
+
+
+socket.on('user-connected', userId =>{
+    console.log('User connected: ' + userId);
+}) 
+
+function addVideoStream(video, stream){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    });
+    
+    videoGrid.append(video)
+}
