a9dfda215ad1b60cfc82887e14cfb7aaf9782375
Madugba Emmanuel Chinonso <nwadubaemmanuel@gmail.com>
2021-11-19 08:54:24 -0800

    :rocket: Improve room video stream

--- public/script.js
+++ public/script.js
@@ -14,7 +14,18 @@ if(navigator.mediaDevices.getUserMedia){
         video: true,
         audio: true
       }).then((stream) => {
-        addVideoStream(myVideo, stream)
+          
+        myPeer.on('call', call => {
+            call.answer(stream)
+            const video = document.createElement('video')
+            call.on('stream', userVideoStream => {
+              addVideoStream(video, userVideoStream)
+            })
+          })
+        
+          socket.on('user-connected', userId => {
+            connectToNewUser(userId, stream)
+          })
     }).catch(err=>{
         console.log(err)
     })
@@ -22,14 +33,25 @@ if(navigator.mediaDevices.getUserMedia){
 }
 
 
-socket.emit('join-room', ROOM_ID, 10)
+myPeer.on('open', id=>{
+
+    socket.emit('join-room', ROOM_ID, id)
+
+})
 
 
-
-
-socket.on('user-connected', userId =>{
-    console.log('User connected: ' + userId);
-}) 
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+      video.remove()
+    })
+  
+    peers[userId] = call
+}
 
 function addVideoStream(video, stream){
     video.srcObject = stream
